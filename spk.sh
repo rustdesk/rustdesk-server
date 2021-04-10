83f97a85fb5a8e45fa8a59bb425f62571c250a07
rustdesk <info@rustdesk.com>
2021-04-11 01:34:26 +0800

    spk

--- spk.sh
+++ spk.sh
@@ -6,8 +6,8 @@ cd package
 mkdir bin logs config
 echo port=21116 > config/hbbs.conf
 echo key= >> config/hbbs.conf
+echo email= >> config/hbbs.conf
 echo port=21117 > config/hbbr.conf
-echo key= >> config/hbbr.conf
 cp ../target/release/hbbs bin/
 cp ../target/release/hbbr bin/
 strip bin/hbbs
@@ -15,6 +15,5 @@ strip bin/hbbr
 tar czf ../spk/package.tgz ./*
 cd ..
 cd spk
-VER=1.1.3
-tar cf RustDeskServer-x64-$VER.spk ./*
-mv RustDeskServer-x64-$VER.spk ..
+tar cf rustdesk-server-synology.spk ./*
+mv rustdesk-server-synology.spk ..
