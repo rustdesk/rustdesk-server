46a7c025a0b587c1721ea67a2c1608a4d83a9a33
rustdesk <info@rustdesk.com>
2023-01-10 22:56:28 +0800

    update version

--- src/version.rs
+++ src/version.rs
@@ -1,3 +1,3 @@
-pub const VERSION: &str = "1.1.6";
+pub const VERSION: &str = "1.1.7";
 #[allow(dead_code)]
-pub const BUILD_DATE: &str = "2023-01-07 11:59";
+pub const BUILD_DATE: &str = "2023-01-10 22:43";
