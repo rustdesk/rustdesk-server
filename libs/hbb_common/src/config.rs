2830be95a75e8cdd59c65697d2e77762982e305b
rustdesk <info@rustdesk.com>
2023-01-27 11:37:43 +0800

    opt

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -87,7 +87,7 @@ pub const RENDEZVOUS_SERVERS: &[&str] = &[
     "rs-cn.rustdesk.com",
 ];
 
-pub const RS_PUB_KEY: &'static str = match option_env!("RS_PUB_KEY") {
+pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
     _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
 };
