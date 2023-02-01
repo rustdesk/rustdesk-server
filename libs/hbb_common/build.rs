fe3b42809ab1f50ee0529c997c0d5ad122b0e6f2
rustdesk <info@rustdesk.com>
2023-02-01 10:49:16 +0800

    run gen_version no matter debug or release

--- libs/hbb_common/build.rs
+++ libs/hbb_common/build.rs
@@ -2,7 +2,7 @@ fn main() {
     let out_dir = format!("{}/protos", std::env::var("OUT_DIR").unwrap());
 
     std::fs::create_dir_all(&out_dir).unwrap();
-    
+
     protobuf_codegen::Codegen::new()
         .pure()
         .out_dir(out_dir)
