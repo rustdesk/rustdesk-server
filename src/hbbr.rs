0e50a707fb34cd13955310f08bc6377f444af2e7
n-connect <info@nconnect.hu>
2023-03-31 00:30:31 +0200

    Fix logger core dump on FreeBSD hbbr
    Flexi_logger core dumps with WriteMode::Async on FreeBSD is a non-bash shell used to run the hbbr binary

--- src/hbbr.rs
+++ src/hbbr.rs
@@ -1,17 +1,26 @@
 use clap::App;
 mod common;
 mod relay_server;
+use std::path::Path;
 use flexi_logger::*;
 use hbb_common::{config::RELAY_PORT, ResultType};
 use relay_server::*;
 mod version;
 
 fn main() -> ResultType<()> {
-    let _logger = Logger::try_with_env_or_str("info")?
-        .log_to_stdout()
-        .format(opt_format)
-        .write_mode(WriteMode::Async)
-        .start()?;
+    if Path::new("/etc/freebsd-update.conf").is_file() {
+         let _logger = Logger::try_with_env_or_str("info")?
+             .log_to_stdout()
+             .format(opt_format)
+             .write_mode(WriteMode::Direct)
+             .start()?;
+     } else {
+         let _logger = Logger::try_with_env_or_str("info")?
+             .log_to_stdout()
+             .format(opt_format)
+             .write_mode(WriteMode::Async)
+             .start()?;
+     }
     let args = format!(
         "-p, --port=[NUMBER(default={RELAY_PORT})] 'Sets the listening port'
         -k, --key=[KEY] 'Only allow the client with the same key'
