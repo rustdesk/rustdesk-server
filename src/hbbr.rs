8a9fd7021b5ab888cd324f2a973838d41bcb550b
n-connect <info@nconnect.hu>
2023-04-01 10:35:16 +0200

    Revert "Fix logger core dump on FreeBSD hbbr"
    This reverts commit 0e50a707fb34cd13955310f08bc6377f444af2e7.

--- src/hbbr.rs
+++ src/hbbr.rs
@@ -1,26 +1,17 @@
 use clap::App;
 mod common;
 mod relay_server;
-use std::path::Path;
 use flexi_logger::*;
 use hbb_common::{config::RELAY_PORT, ResultType};
 use relay_server::*;
 mod version;
 
 fn main() -> ResultType<()> {
-    if Path::new("/etc/freebsd-update.conf").is_file() {
-         let _logger = Logger::try_with_env_or_str("info")?
-             .log_to_stdout()
-             .format(opt_format)
-             .write_mode(WriteMode::Direct)
-             .start()?;
-     } else {
-         let _logger = Logger::try_with_env_or_str("info")?
-             .log_to_stdout()
-             .format(opt_format)
-             .write_mode(WriteMode::Async)
-             .start()?;
-     }
+    let _logger = Logger::try_with_env_or_str("info")?
+        .log_to_stdout()
+        .format(opt_format)
+        .write_mode(WriteMode::Async)
+        .start()?;
     let args = format!(
         "-p, --port=[NUMBER(default={RELAY_PORT})] 'Sets the listening port'
         -k, --key=[KEY] 'Only allow the client with the same key'
